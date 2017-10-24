fc6c79cdcd18170e591906b4df4ccd97b3e37e95
Bernardo <bernardofvalente@gmail.com>
2017-10-24 14:29:33 +0100

    P3 sequencial ja tem terminacao dinamica

--- P3/sequencial/main.c
+++ P3/sequencial/main.c
@@ -14,11 +14,11 @@
 | Function: simul
 ---------------------------------------------------------------------*/
 
-DoubleMatrix2D *simul(DoubleMatrix2D *matrix, DoubleMatrix2D *matrix_aux, int linhas, int colunas, int numIteracoes) {
+DoubleMatrix2D *simul(DoubleMatrix2D *matrix, DoubleMatrix2D *matrix_aux, int linhas, int colunas, int numIteracoes, double MaxD) {
 
   DoubleMatrix2D *m, *aux, *tmp;
   int iter, i, j;
-  double value;
+  double value, MaxDaux = 0;
 
 
   if(linhas < 2 || colunas < 2)
@@ -28,7 +28,7 @@ DoubleMatrix2D *simul(DoubleMatrix2D *matrix, DoubleMatrix2D *matrix_aux, int li
   aux = matrix_aux /*matrix_aux*/;
 
   for (iter = 0; iter < numIteracoes; iter++) {
-  
+
     for (i = 1; i < linhas - 1; i++)
       for (j = 1; j < colunas - 1; j++) {
         value = ( dm2dGetEntry(m, i-1, j) + dm2dGetEntry(m, i+1, j) +
@@ -39,6 +39,20 @@ DoubleMatrix2D *simul(DoubleMatrix2D *matrix, DoubleMatrix2D *matrix_aux, int li
     tmp = aux;
     aux = m;
     m = tmp;
+
+    //check MaxD
+    for (i = 1; i < linhas - 1; i++){
+      for (j = 1; j < colunas - 1;j++){
+        if (dm2dGetEntry(m,i,j) - dm2dGetEntry(aux,i,j) > MaxDaux)
+          MaxDaux = dm2dGetEntry(m,i,j) - dm2dGetEntry(aux,i,j);
+      }
+    }
+    printf("MaxDaux:%.4f\n",MaxDaux );
+    if (MaxDaux <= MaxD)
+      return m;
+    else
+      MaxDaux = 0;
+
   }
 
   return m;
@@ -51,7 +65,7 @@ DoubleMatrix2D *simul(DoubleMatrix2D *matrix, DoubleMatrix2D *matrix_aux, int li
 int parse_integer_or_exit(char const *str, char const *name)
 {
   int value;
- 
+
   if(sscanf(str, "%d", &value) != 1) {
     fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
     exit(1);
@@ -81,9 +95,9 @@ double parse_double_or_exit(char const *str, char const *name)
 
 int main (int argc, char** argv) {
 
-  if(argc != 7) {
+  if(argc != 8) {
     fprintf(stderr, "\nNumero invalido de argumentos.\n");
-    fprintf(stderr, "Uso: heatSim N tEsq tSup tDir tInf iteracoes\n\n");
+    fprintf(stderr, "Uso: heatSim N tEsq tSup tDir tInf iteracoes MaxD\n\n");
     return 1;
   }
 
@@ -94,13 +108,14 @@ int main (int argc, char** argv) {
   double tDir = parse_double_or_exit(argv[4], "tDir");
   double tInf = parse_double_or_exit(argv[5], "tInf");
   int iteracoes = parse_integer_or_exit(argv[6], "iteracoes");
+  double MaxD = parse_double_or_exit(argv[7], "MaxD");
 
   DoubleMatrix2D *matrix, *matrix_aux, *result;
 
 
   fprintf(stderr, "\nArgumentos:\n"
-	" N=%d tEsq=%.1f tSup=%.1f tDir=%.1f tInf=%.1f iteracoes=%d\n",
-	N, tEsq, tSup, tDir, tInf, iteracoes);
+	" N=%d tEsq=%.1f tSup=%.1f tDir=%.1f tInf=%.1f iteracoes=%d MaxD=%.4f\n",
+	N, tEsq, tSup, tDir, tInf, iteracoes, MaxD);
 
   if(N < 1 || tEsq < 0 || tSup < 0 || tDir < 0 || tInf < 0 || iteracoes < 1) {
     fprintf(stderr, "\nErro: Argumentos invalidos.\n"
@@ -116,32 +131,7 @@ int main (int argc, char** argv) {
     return -1;
   }
 
-
-  /* ALTERACOES PEDIDAS  <inicio>  */
-
   int i;
-
-#if 0
-  for(i=0; i<N+2; i++)
-    dm2dSetLineTo(matrix, i, i);
-  dm2dPrint(matrix);
-
-
-  for(i=0; i<N+2; i++)
-    dm2dSetColumnTo(matrix_aux, i, i);
-  dm2dPrint(matrix_aux);
-
-
-  if(3 <= N+1) {
-    double *line;
-    line = dm2dGetLine(matrix, 3);
-    dm2dSetLine(matrix_aux, 2, line);
-    dm2dPrint(matrix_aux);
-  }
-#endif
-  /* ALTERACOES PEDIDAS  <fim>  */
-
-
   for(i=0; i<N+2; i++)
     dm2dSetLineTo(matrix, i, 0);
 
@@ -152,7 +142,7 @@ int main (int argc, char** argv) {
 
   dm2dCopy (matrix_aux, matrix);
 
-  result = simul(matrix, matrix_aux, N+2, N+2, iteracoes);
+  result = simul(matrix, matrix_aux, N+2, N+2, iteracoes, MaxD);
   if (result == NULL) {
     printf("\nErro na simulacao.\n\n");
     return -1;
