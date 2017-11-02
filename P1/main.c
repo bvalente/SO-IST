63ef00b68c71f18fcc2244688c58fd7b25f3f41a
machadoatz <franciscocmachado@gmail.com>
2017-11-02 10:22:31 +0000

    inicio implementacao block de MemPatilahda

--- P1/main.c
+++ P1/main.c
@@ -1,10 +1,10 @@
 
 /*
-// Projeto SO - exercicio 1, version 03
-// Sistemas Operativos, DEI/IST/ULisboa 2017-18
-
-// Realizado por Bernardo Valente e Francisco Machado
-*/
+ // Projeto SO - exercicio 1, version 03
+ // Sistemas Operativos, DEI/IST/ULisboa 2017-18
+ 
+ // Realizado por Bernardo Valente e Francisco Machado
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,35 +24,38 @@ typedef struct Args{
 }EscravoArgs;
 
 /*--------------------------------------------------------------------
-| Function: simulFatia
----------------------------------------------------------------------*/
+ | Function: simulFatia
+ ---------------------------------------------------------------------*/
 
 void *simulFatia(void* argumentos){
-  EscravoArgs* args = (EscravoArgs*) argumentos;
-  int ID = args->ID;
-  int numIteracoes = args->numIteracoes;
-  int numThreads = args->numThreads;
-  int N = args->N;
-  int linhas = N / numThreads;
-  int colunas = N;
-  int tam = sizeof(double) * (N + 2);
-  int i, l, k; // Variaveis de iteracao
-
-  DoubleMatrix2D *matrix, *matrix_aux, *ponteiro_aux;
-
-  //Alocamento de memoria
-	matrix = dm2dNew(linhas+2, colunas+2);
-	matrix_aux = dm2dNew(linhas+2, colunas+2);
-
-  for ( i = 0; i < (linhas + 2); i++){
-    receberMensagem(0, ID, dm2dGetLine(matrix, i) , tam );
-  }
-
-  dm2dCopy(matrix_aux,matrix);
-
-	double value;
-	for (i = 0; i < numIteracoes; i++){
-
+    EscravoArgs* args = (EscravoArgs*) argumentos;
+    int ID = args->ID;
+    int numIteracoes = args->numIteracoes;
+    int numThreads = args->numThreads;
+    int N = args->N;
+    int linhas = N / numThreads;
+    int colunas = N;
+    int tam = sizeof(double) * (N + 2);
+    int i, l, k; // Variaveis de iteracao
+    
+    DoubleMatrix2D *matrix, *matrix_aux, *ponteiro_aux;
+    
+    //Alocamento de memoria
+    matrix = dm2dNew(linhas+2, colunas+2);
+    matrix_aux = dm2dNew(linhas+2, colunas+2);
+    
+    for ( i = 0; i < (linhas + 2); i++){
+        receberMensagem(0, ID, dm2dGetLine(matrix, i) , tam );
+    }
+    
+    dm2dCopy(matrix_aux,matrix);
+    
+    receberMensagem(0,ID, s, sizeof(char)*8));
+    fprintf("%s", s);
+    
+    double value;
+    for (i = 0; i < numIteracoes; i++){
+        
         //Calculos dos valores
         for (l = 1; l <= linhas ; l++){
             for (k = 1; k <= colunas ; k++) {
@@ -60,177 +63,182 @@ void *simulFatia(void* argumentos){
                 dm2dSetEntry(matrix_aux, l, k, value);
             }
         }
-
-    ponteiro_aux = matrix;
-    matrix = matrix_aux;
-    matrix_aux = ponteiro_aux;
-
-    //envio de mensagens
-    if ( numThreads != 1 ){
-      if ( ID == 1){
-        enviarMensagem( ID, ID + 1, dm2dGetLine(matrix, linhas), tam);
-        receberMensagem( ID + 1, ID, dm2dGetLine(matrix, linhas + 1), tam);
-      }
-    	else if ( ID == numThreads){
-        enviarMensagem (ID, ID - 1, dm2dGetLine(matrix, 1), tam);
-        receberMensagem( ID - 1, ID, dm2dGetLine(matrix, 0), tam);
-      }
-    	else {
-        enviarMensagem (ID, ID + 1, dm2dGetLine(matrix, linhas), tam);
-        enviarMensagem (ID, ID - 1, dm2dGetLine(matrix, 1), tam);
-
-        receberMensagem( ID + 1, ID, dm2dGetLine(matrix, linhas + 1), tam);
-        receberMensagem( ID - 1, ID, dm2dGetLine(matrix, 0), tam);
-      }
+        
+        ponteiro_aux = matrix;
+        matrix = matrix_aux;
+        matrix_aux = ponteiro_aux;
+        
+        //envio de mensagens
+        if ( numThreads != 1 ){
+            if ( ID == 1){
+                enviarMensagem( ID, ID + 1, dm2dGetLine(matrix, linhas), tam);
+                receberMensagem( ID + 1, ID, dm2dGetLine(matrix, linhas + 1), tam);
+            }
+            else if ( ID == numThreads){
+                enviarMensagem (ID, ID - 1, dm2dGetLine(matrix, 1), tam);
+                receberMensagem( ID - 1, ID, dm2dGetLine(matrix, 0), tam);
+            }
+            else {
+                enviarMensagem (ID, ID + 1, dm2dGetLine(matrix, linhas), tam);
+                enviarMensagem (ID, ID - 1, dm2dGetLine(matrix, 1), tam);
+                
+                receberMensagem( ID + 1, ID, dm2dGetLine(matrix, linhas + 1), tam);
+                receberMensagem( ID - 1, ID, dm2dGetLine(matrix, 0), tam);
+            }
+        }
     }
-  }
-
-  //enviar mensagem para a main
-
-  for ( i = 0; i < (linhas + 2); i++){
-    enviarMensagem(ID, 0, dm2dGetLine(matrix, i) , tam );
-  }
-  //free
-  dm2dFree(matrix);
-  dm2dFree(matrix_aux);
-
-  return NULL;
+    
+    //enviar mensagem para a main
+    
+    for ( i = 0; i < (linhas + 2); i++){
+        enviarMensagem(ID, 0, dm2dGetLine(matrix, i) , tam );
+    }
+    //free
+    dm2dFree(matrix);
+    dm2dFree(matrix_aux);
+    
+    return NULL;
 }
 
 /*--------------------------------------------------------------------
-| Function: parse_integer_or_exit
----------------------------------------------------------------------*/
+ | Function: parse_integer_or_exit
+ ---------------------------------------------------------------------*/
 
 int parse_integer_or_exit(char const *str, char const *name)
 {
-  int value;
-
-  if(sscanf(str, "%d", &value) != 1) {
-    fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
-    exit(1);
-  }
-  return value;
+    int value;
+    
+    if(sscanf(str, "%d", &value) != 1) {
+        fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
+        exit(1);
+    }
+    return value;
 }
 
 /*--------------------------------------------------------------------
-| Function: parse_double_or_exit
----------------------------------------------------------------------*/
+ | Function: parse_double_or_exit
+ ---------------------------------------------------------------------*/
 
 double parse_double_or_exit(char const *str, char const *name)
 {
-  double value;
-
-  if(sscanf(str, "%lf", &value) != 1) {
-    fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
-    exit(1);
-  }
-  return value;
+    double value;
+    
+    if(sscanf(str, "%lf", &value) != 1) {
+        fprintf(stderr, "\nErro no argumento \"%s\".\n\n", name);
+        exit(1);
+    }
+    return value;
 }
 
 
 /*--------------------------------------------------------------------
-| Function: main
----------------------------------------------------------------------*/
+ | Function: main
+ ---------------------------------------------------------------------*/
 
 int main (int argc, char** argv) {
-
-  if(argc != 9) {
-    fprintf(stderr, "\nNumero invalido de argumentos.\n");
-    fprintf(stderr, "Uso: heatSim N tEsq tSup tDir tInf iteracoes trab csz\n\n");
-    return 1;
-  }
-
-  int N = parse_integer_or_exit(argv[1], "N");
-  double tEsq = parse_double_or_exit(argv[2], "tEsq");
-  double tSup = parse_double_or_exit(argv[3], "tSup");
-  double tDir = parse_double_or_exit(argv[4], "tDir");
-  double tInf = parse_double_or_exit(argv[5], "tInf");
-  int iteracoes = parse_integer_or_exit(argv[6], "iteracoes");
-  int trab = parse_integer_or_exit(argv[7], "trab");
-  int csz = parse_integer_or_exit(argv[8], "csz");
-
-
-  DoubleMatrix2D *matrix;
-
-
-  fprintf(stderr, "\nArgumentos:\n"
-	" N=%d tEsq=%.1f tSup=%.1f tDir=%.1f tInf=%.1f iteracoes=%d trabalhos=%d csz=%d\n",
-	N, tEsq, tSup, tDir, tInf, iteracoes, trab, csz);
-
-  if(N < 1 || tEsq < 0 || tSup < 0 || tDir < 0 || tInf < 0 || iteracoes < 1 || trab < 1 || csz < 1) {
-    fprintf(stderr, "\nErro: Argumentos invalidos.\n"
-	" Lembrar que N >= 1, temperaturas >= 0, iteracoes >= 1, trab >= 1 e csz >= 1\n\n");
-    return 1;
-  }
-
-  //verificar se trab e valido
-  if ( N % trab != 0){
-	  fprintf(stderr, "\nErro: N nao e divisivel por trab.\n\n");
-	  return 1;
-  }
-
-  inicializarMPlib(csz,  trab+1);
-
-  matrix = dm2dNew(N+2, N+2);
-
-  if (matrix == NULL) {
-    fprintf(stderr, "\nErro: Nao foi possivel alocar memoria para a matriz.\n\n");
-    return -1;
-  }
-
-  int ID, i;
-
-  for(i=0; i<N+2; i++)
-      dm2dSetLineTo(matrix, i, 0);
     
-  dm2dSetLineTo (matrix, 0, tSup);
-  dm2dSetLineTo (matrix, N+1, tInf);
-  dm2dSetColumnTo (matrix, 0, tEsq);
-  dm2dSetColumnTo (matrix, N+1, tDir);
-
-  pthread_t *tid = malloc( sizeof(pthread_t) * trab);
-  EscravoArgs *argumentos = malloc( sizeof(EscravoArgs) * trab);
-
-  for(i=1; i <= trab; i++){
-      
-    argumentos[i-1].ID = i;
-    argumentos[i-1].numIteracoes = iteracoes;
-    argumentos[i-1].numThreads = trab;
-    argumentos[i-1].N = N;
-
-    pthread_create(&tid[i-1], NULL, simulFatia, &argumentos[i-1]);
-  }
-
-  int tamFatia = N/trab , j;
-
-  //enviar mensagens para as threads
-  j = 0;
+    if(argc != 9) {
+        fprintf(stderr, "\nNumero invalido de argumentos.\n");
+        fprintf(stderr, "Uso: heatSim N tEsq tSup tDir tInf iteracoes trab csz\n\n");
+        return 1;
+    }
     
-  for  ( ID = 1; ID <= trab; ID++ ){
-    for ( i = j; i < j + tamFatia + 2; i++){
-      enviarMensagem(0, ID, dm2dGetLine(matrix, i), sizeof(double)* (N+2) ) ;
+    int N = parse_integer_or_exit(argv[1], "N");
+    double tEsq = parse_double_or_exit(argv[2], "tEsq");
+    double tSup = parse_double_or_exit(argv[3], "tSup");
+    double tDir = parse_double_or_exit(argv[4], "tDir");
+    double tInf = parse_double_or_exit(argv[5], "tInf");
+    int iteracoes = parse_integer_or_exit(argv[6], "iteracoes");
+    int trab = parse_integer_or_exit(argv[7], "trab");
+    int csz = parse_integer_or_exit(argv[8], "csz");
+    
+    
+    DoubleMatrix2D *matrix;
+    
+    
+    fprintf(stderr, "\nArgumentos:\n"
+            " N=%d tEsq=%.1f tSup=%.1f tDir=%.1f tInf=%.1f iteracoes=%d trabalhos=%d csz=%d\n",
+            N, tEsq, tSup, tDir, tInf, iteracoes, trab, csz);
+    
+    if(N < 1 || tEsq < 0 || tSup < 0 || tDir < 0 || tInf < 0 || iteracoes < 1 || trab < 1 || csz < 1) {
+        fprintf(stderr, "\nErro: Argumentos invalidos.\n"
+                " Lembrar que N >= 1, temperaturas >= 0, iteracoes >= 1, trab >= 1 e csz >= 1\n\n");
+        return 1;
     }
-    j += tamFatia;
-  }
-
-  //Receber as mensagens de todas as threads e constroi matriz
-  j = 0;
-  for  ( ID = 1; ID <= trab; ID++ ){
-    for ( i = j; i < j + tamFatia + 2; i++){
-      receberMensagem(ID, 0, dm2dGetLine(matrix, i), sizeof(double)* (N+2) ) ;
+    
+    //verificar se trab e valido
+    if ( N % trab != 0){
+        fprintf(stderr, "\nErro: N nao e divisivel por trab.\n\n");
+        return 1;
     }
-    j += tamFatia;
-  }
-
-  for(i=1; i <= trab; i++){
-    pthread_join(tid[i-1], NULL);
-  }
-
-  dm2dPrint(matrix);
-  dm2dFree(matrix);
-  free(tid);
-  free(argumentos);
-  libertarMPlib();
-
-  return 0;
+    
+    inicializarMPlib(csz,  trab+1);
+    
+    matrix = dm2dNew(N+2, N+2);
+    
+    if (matrix == NULL) {
+        fprintf(stderr, "\nErro: Nao foi possivel alocar memoria para a matriz.\n\n");
+        return -1;
+    }
+    
+    int ID, i;
+    
+    for(i=0; i<N+2; i++)
+        dm2dSetLineTo(matrix, i, 0);
+    
+    dm2dSetLineTo (matrix, 0, tSup);
+    dm2dSetLineTo (matrix, N+1, tInf);
+    dm2dSetColumnTo (matrix, 0, tEsq);
+    dm2dSetColumnTo (matrix, N+1, tDir);
+    
+    pthread_t *tid = malloc( sizeof(pthread_t) * trab);
+    EscravoArgs *argumentos = malloc( sizeof(EscravoArgs) * trab);
+    
+    for(i=1; i <= trab; i++){
+        
+        argumentos[i-1].ID = i;
+        argumentos[i-1].numIteracoes = iteracoes;
+        argumentos[i-1].numThreads = trab;
+        argumentos[i-1].N = N;
+        
+        pthread_create(&tid[i-1], NULL, simulFatia, &argumentos[i-1]);
+    }
+    
+    int tamFatia = N/trab , j;
+    
+    //enviar mensagens para as threads
+    j = 0;
+    
+    for  ( ID = 1; ID <= trab; ID++ ){
+        for ( i = j; i < j + tamFatia + 2; i++){
+            enviarMensagem(0, ID, dm2dGetLine(matrix, i), sizeof(double)* (N+2) ) ;
+        }
+        j += tamFatia;
+    }
+    char *s = 'Pronto!\n';
+    for (ID=1; ID <=trab; ID++){
+        enviarMensagem(0,ID,s, sizeof(char)*8);
+    }
+    
+    
+    //Receber as mensagens de todas as threads e constroi matriz
+    j = 0;
+    for  ( ID = 1; ID <= trab; ID++ ){
+        for ( i = j; i < j + tamFatia + 2; i++){
+            receberMensagem(ID, 0, dm2dGetLine(matrix, i), sizeof(double)* (N+2) ) ;
+        }
+        j += tamFatia;
+    }
+    
+    for(i=1; i <= trab; i++){
+        pthread_join(tid[i-1], NULL);
+    }
+    
+    dm2dPrint(matrix);
+    dm2dFree(matrix);
+    free(tid);
+    free(argumentos);
+    libertarMPlib();
+    
+    return 0;
 }
---
+++ P3/mensagens/mplib4.c
@@ -0,0 +1,289 @@
+/*
+// Biblioteca de troca de mensagens, versao 4
+// Sistemas Operativos, DEI/IST/ULisboa 2017-18
+*/
+
+#include "mplib4.h"
+#include "leQueue.h"
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+
+
+/*--------------------------------------------------------------------
+| Types
+---------------------------------------------------------------------*/
+
+typedef struct message_t {
+  QueElem   elem;            
+  void      *contents;
+  int       mess_size;
+  int       consumed;
+} Message_t;
+
+typedef struct channel_t {
+  QueHead           *message_list;
+  pthread_mutex_t   mutex;
+  pthread_cond_t    wait_for_free_space;
+  pthread_cond_t    wait_for_messages;
+} Channel_t;
+
+
+/*--------------------------------------------------------------------
+| Global Variables
+---------------------------------------------------------------------*/
+
+int                channel_capacity;
+int                number_of_tasks;
+Channel_t          **channel_array;
+
+/*--------------------------------------------------------------------
+  | Function: createChannel
+  | Description: Cria um canal de comunicação com suporte para uma lista
+  | de mensagens e um mutex para síncronização dessa lista.
+  ---------------------------------------------------------------------*/
+
+Channel_t* createChannel () {
+  Channel_t     *channel = (Channel_t*) malloc (sizeof(Channel_t));
+
+  if (channel == NULL) {
+    fprintf(stderr, "\nErro ao criar canal\n");
+    exit(1);
+  }
+
+  channel->message_list = leQueNewHead();
+
+  if (channel->message_list == NULL) {
+    fprintf(stderr, "\nErro ao criar lista de mensagens\n");
+    exit(1);
+  }
+
+  if(pthread_mutex_init(&(channel->mutex), NULL) != 0) {
+    fprintf(stderr, "\nErro ao inicializar mutex\n");
+    exit(1);
+  }
+
+  if(pthread_cond_init(&(channel->wait_for_free_space), NULL) != 0) {
+    fprintf(stderr, "\nErro ao inicializar variável de condição\n");
+    exit(1);
+  }
+
+  if(pthread_cond_init(&(channel->wait_for_messages), NULL) != 0){
+    fprintf(stderr, "\nErro ao inicializar variável de condição\n");
+    exit(1);
+  }
+
+  leQueHeadInit (channel->message_list, 0);
+  
+  return channel;
+}
+
+/*--------------------------------------------------------------------
+  | Function: inicializarMPlib
+  | Description: Inicializa a API de troca de mensagens
+  ---------------------------------------------------------------------*/
+
+int inicializarMPlib(int capacidade_de_cada_canal, int ntasks) {
+  int           i;
+  Channel_t     *channel;
+
+  number_of_tasks  = ntasks;
+  channel_capacity = capacidade_de_cada_canal;
+  channel_array    = (Channel_t**) malloc (sizeof(Channel_t*)*ntasks*ntasks);
+  
+  if (channel_array == NULL) {
+    fprintf(stderr, "\nErro ao inicializar MPlib\n");
+    exit(1);
+  }
+ 
+  for (i = 0; i < ntasks * ntasks; i++) {
+    channel = createChannel();
+    if (channel == NULL)
+      exit(1);
+
+    channel_array[i] = channel;
+  }
+  
+  return 0;
+}
+
+
+/*--------------------------------------------------------------------
+  | Function: libertarMPlib
+  | Description: Liberta a memória usada por esta API
+  ---------------------------------------------------------------------*/
+
+void libertarMPlib() {
+  int i;
+
+  for (i = 0; i < number_of_tasks * number_of_tasks; ++i) {
+    Channel_t   *channel = channel_array[i];
+    Message_t   *mess    = (Message_t*) leQueRemFirst(channel->message_list);
+
+    while (mess) {
+      if (channel_capacity > 0)
+        free (mess->contents);
+      free (mess);
+      mess = (Message_t*) leQueRemFirst(channel->message_list);
+    }
+
+    if(pthread_mutex_destroy(&(channel->mutex)) != 0) {
+      fprintf(stderr, "\nErro ao destruir mutex\n");
+      exit(1);
+    }
+  
+    if(pthread_cond_destroy(&(channel->wait_for_free_space)) != 0) {
+      fprintf(stderr, "\nErro ao destruir variável de condição\n");
+      exit(1);
+    }
+
+    if(pthread_cond_destroy(&(channel->wait_for_messages)) != 0) {
+      fprintf(stderr, "\nErro ao destruir variável de condição\n");
+      exit(1);
+    }
+
+    free (channel);
+  }
+ 
+  free (channel_array);
+}
+
+
+/*--------------------------------------------------------------------
+  | Function: receberMensagem
+  | Description: Lê uma mensagem que esteja pendente na lista de
+  | mensagens do canal ou bloqueia a tarefa que chama a função enquanto
+  | não há mensagens para ler.
+  ---------------------------------------------------------------------*/
+
+int receberMensagem(int tarefaOrig, int tarefaDest, void *buffer, int tamanho) {
+  Channel_t      *channel;
+  Message_t      *mess;
+  int            copysize;
+
+  channel = (Channel_t*) channel_array[tarefaDest*number_of_tasks+tarefaOrig];
+  
+  if(pthread_mutex_lock(&(channel->mutex)) != 0) {
+    fprintf(stderr, "\nErro ao bloquear mutex\n");
+    exit(1);
+  }
+
+  mess    = (Message_t*) leQueRemFirst (channel->message_list);
+
+  while (!mess) {
+    if(pthread_cond_wait(&(channel->wait_for_messages), &(channel->mutex)) != 0) {
+        fprintf(stderr, "\nErro ao esperar pela variável de condição\n");
+        exit(1);
+    }
+    mess = (Message_t*) leQueRemFirst (channel->message_list);
+  }
+    
+  copysize = (mess->mess_size<tamanho) ? mess->mess_size : tamanho;
+  memcpy(buffer, mess->contents, copysize);
+  
+  if (channel_capacity > 0) {
+    free(mess->contents);
+    free(mess);
+  }
+  else {
+    mess->consumed = 1;
+  }
+  
+  if (pthread_cond_signal(&(channel->wait_for_free_space)) != 0) {
+    fprintf(stderr, "\nErro ao desbloquear variável de condição\n");
+    exit(1);
+  }
+
+  if (pthread_mutex_unlock(&(channel->mutex)) != 0) {
+    fprintf(stderr, "\nErro ao desbloquear mutex\n");
+    exit(1);
+  }
+
+  return copysize;
+}
+
+/*--------------------------------------------------------------------
+  | Function: enviarMensagem
+  | Description: Envia uma mensagem pelo canal correspondente caso
+  | não exceda a capacidade do canal. Caso exceda, a tarefa que
+  | chamou a função espera.
+  ---------------------------------------------------------------------*/
+
+int enviarMensagem(int tarefaOrig, int tarefaDest, void *msg, int tamanho) {
+  Channel_t     *channel;
+  Message_t     *mess;
+
+  channel = (Channel_t*) channel_array[tarefaDest*number_of_tasks+tarefaOrig];
+  mess = (Message_t*) malloc (sizeof(Message_t));
+
+  if (mess == NULL) {
+    fprintf(stderr, "\nErro ao alocar memória para mensagem\n");
+    exit(1);
+  }
+
+  leQueElemInit (mess);
+  mess->mess_size = tamanho;
+
+  // If channels are buffered, copy message to a temporary buffer
+  if (channel_capacity > 0) {
+    mess->contents = malloc (tamanho);
+    mess->consumed = 0;
+
+    if(mess->contents == NULL) {
+      fprintf(stderr, "\nErro ao alocar memória para o conteúdo da mensagem\n");
+      exit(1);
+    }
+    
+    memcpy(mess->contents, msg, tamanho);
+  }
+  // if channels are not buffered, message will be copied directly from sender to receiver
+  else {
+    mess->contents = msg;
+    mess->consumed = 0;
+  }
+  
+  if (pthread_mutex_lock(&(channel->mutex)) != 0) {
+    fprintf(stderr, "\nErro ao bloquear mutex\n");
+    exit(1);
+  }
+
+  // if channels are buffered, wait until there is buffer available
+  if (channel_capacity > 0) {
+    while (leQueSize(channel->message_list) >= channel_capacity) {
+      if (pthread_cond_wait(&(channel->wait_for_free_space), &(channel->mutex)) != 0) {
+        fprintf(stderr, "\nErro ao esperar pela variável de condição\n");
+        exit(1);
+      }
+    }
+  }
+  
+  leQueInsLast (channel->message_list, mess);
+
+  if (pthread_cond_signal(&(channel->wait_for_messages)) != 0) {
+    fprintf(stderr, "\nErro ao desbloquear variável de condição\n");
+    exit(1);
+  }
+
+  // if channels are not buffered, wait for message to be read
+  if (channel_capacity == 0) {
+    while (!mess->consumed) {
+      if(pthread_cond_wait(&(channel->wait_for_free_space), &(channel->mutex)) != 0) {
+        fprintf(stderr, "\nErro ao esperar pela variável de condição\n");
+        exit(1);
+      }
+    
+    }
+    free(mess);
+  }
+  
+  if(pthread_mutex_unlock(&(channel->mutex)) != 0) {
+    fprintf(stderr, "\nErro ao desbloquear mutex\n");
+    exit(1);
+  }
+
+  return tamanho;
+}
